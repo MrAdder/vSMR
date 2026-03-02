c390ef631ebec7e5b5988cf2ca74cef2e25703c7
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2026-03-02 19:34:11 +0000

    Update plugin_version.ps1

--- .github/workflows/plugin_version.ps1
+++ .github/workflows/plugin_version.ps1
@@ -2,6 +2,11 @@ Function Build-Version {
     # If we're on main, invoke semantic release to get the next version
     if (${env:GITHUB_REF} -eq "refs/heads/main" -or ${env:GITHUB_REF} -eq "refs/heads/master" -or ${env:GITHUB_REF} -eq "refs/heads/alpha" -or ${env:GITHUB_REF} -eq "refs/heads/beta")
     {
+        if (-not (Test-Path ".\\package.json"))
+        {
+            return "non-release-build"
+        }
+
         $semanticRelease = yarn semantic-release --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
         if ($semanticRelease.Matches -eq $null)
         {
@@ -19,5 +24,12 @@ Function Build-Version {
 
 $buildVersion = Build-Version;
 echo "Building version $buildVersion"
-$versionFile = ".\\src\\utils\\update\\PluginVersion.cpp"
-(Get-Content $versionFile).replace("#VERSION_STRING#", "$buildVersion") | Set-Content $versionFile
+$versionFile = ".\\vSMR\\SMRPlugin.hpp"
+$pattern = '(^\s*#define\s+MY_PLUGIN_VERSION\s+").*("\s*$)'
+
+if (-not (Test-Path $versionFile))
+{
+    throw "Cannot find plugin version file at $versionFile"
+}
+
+(Get-Content $versionFile) -replace $pattern, "`$1$buildVersion`$2" | Set-Content $versionFile
