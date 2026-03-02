98037eac5a4d74b689570da1e70b2d8b889e7f22
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2026-03-02 18:30:34 +0000

    Create plugin_version.ps1

---
+++ .github/plugin_version.ps1
@@ -0,0 +1,23 @@
+Function Build-Version {
+    # If we're on main, invoke semantic release to get the next version
+    if (${env:GITHUB_REF} -eq "refs/heads/main" -or ${env:GITHUB_REF} -eq "refs/heads/alpha" -or ${env:GITHUB_REF} -eq "refs/heads/beta")
+    {
+        $semanticRelease = yarn semantic-release --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
+        if ($semanticRelease.Matches -eq $null)
+        {
+            return "non-release-build"
+        }
+        else
+        {
+            return $semanticRelease.Matches.Groups[1].value;
+        }
+    } else
+    {
+        return "non-release-build"
+    }
+}
+
+$buildVersion = Build-Version;
+echo "Building version $buildVersion"
+$versionFile = ".\\src\\utils\\update\\PluginVersion.cpp"
+(Get-Content $versionFile).replace("#VERSION_STRING#", "$buildVersion") | Set-Content $versionFile
